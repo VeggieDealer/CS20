f124082a88be569e07b97d73c468c80801547097
VeggieDealer <31700978+VeggieDealer@users.noreply.github.com>
2017-09-06 10:43:08 -0600

    array basic

---
+++ P5JS Template - Basic/sketch.js
@@ -0,0 +1,18 @@
+// SETUP FUNCTION - Runs once at beginning of program
+var sploog = [];
+
+function setup() {
+    createCanvas(800, 600);
+	background("white");
+
+  for(x = 0; x < 8; x ++) {
+    sploog.push([])
+    for (y = 0; y < 7; y ++) {
+
+    }
+  }
+}
+
+// DRAW FUNCTION - Loops @ 60FPS by default
+function draw() {
+}
