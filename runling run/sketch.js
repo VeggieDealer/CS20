2713ddf9bbbac141258b178ee7047ed32aedc61a
VeggieDealer <31700978+VeggieDealer@users.noreply.github.com>
2017-09-27 10:45:19 -0600

    its actually working?

--- runling run/sketch.js
+++ runling run/sketch.js
@@ -1,10 +1,24 @@
 var runling = {
   x: 10,
   y: 10,
-  xSpeed: 1,
-  ySpeed: 1,
+  xSpeed: 0.5,
+  ySpeed: 0.5,
+}
+var runlingMove = false;
+var runlingDestination = {
+  x: 0,
+  y: 0
 }
 
+drones = [];
+baseDrone = {
+  x: 0,
+  y: 0,
+  r: 10,
+  xMove: 2,
+  yMove: 2
+}
+var droneNumber = 0;
 
 // SETUP FUNCTION - Runs once at beginning of program
 function setup() {
@@ -17,17 +31,42 @@ function draw() {
   fill(200, 200, 200);
   drawMap();
   drawCharacter();
+
+
+
+  if (droneNumber < 20 ) {
+      drones.push(Object.assign({}, baseDrone));
+      drones[drones.length - 1].x = random(75, 950);
+      drones[drones.length - 1].y = random(0, 70);
+      droneNumber ++;
+  }
+
+  for (i = 0; i < drones.length; i ++) {
+    ellipse(drones[i].x, drones[i].y, drones[i].r);
+
+    if (dist(runling.x, runling.y, drones[i].x, drones[i].y) < drones[i].r) {
+      runling.x = 10;
+      runling.y = 10;
+      runlingMove = false;
+    }
+  }
+
+  if (runlingMove) {
+    if(runling.x < runlingDestination.x) {
+      runling.x += runling.xSpeed;
+    } else if (runling.x > runlingDestination.x) {
+      runling.x -= runling.xSpeed;
+    }
+    if (runling.y < runlingDestination.y) {
+      runling.y += runling.ySpeed;
+    } else if (runling.y > runlingDestination.y) {
+      runling.y -= runling.ySpeed;
+    }
+  }
 }
 
 function mousePressed() {
-  if (runling.x < mouseX) {
-    runling.x += runling.xSpeed;
-  } else if (runling.x > mouseX)  {
-    runling.x += runling.xSpeed*-1;
-  }
-  if (runling.y < mouseY) {
-    runling.y += runling.ySpeed;
-  } else if (runling.x > mouseY)  {
-    runling.y += runling.ySpeed*-1;
-  }
+  runlingMove = true;
+  runlingDestination.x = mouseX;
+  runlingDestination.y = mouseY;
 }
